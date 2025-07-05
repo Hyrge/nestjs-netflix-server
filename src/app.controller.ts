ca37cca75b1402b7a451e8be317870f6962a6c41
Hyrge <ekek7478@gmail.com>
2025-07-06 03:46:19 +0900

    feat: 영화 관리 API 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,67 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, NotFoundException } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Movie {
+  id: number;
+  title: string;
+};
+
+@Controller('movie')
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
+  private movies: Movie[] = [
+    { id: 1, title: '인셉션' },
+    { id: 2, title: '인터스텔라' },
+  ];
+
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return this.movies;
   }
+
+  @Get(':id')
+  getMovie(@Param('id', ParseIntPipe) id: number) {
+    const movie = this.movies.find(m => m.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found`);
+    }
+    return movie;
+  }
+
+  @Post()
+  createMovie(@Body() body: { title: string }) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      title: body.title,
+    });
+    return this.movies;
+  }
+
+  @Patch(':id')
+  updateMovie(@Param('id', ParseIntPipe) id: number, @Body() body: { title: string }) {
+    const movie = this.movies.find(m => m.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found`);
+    }
+    movie.title = body.title;
+    return movie;
+  
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id', ParseIntPipe) id: number) {
+    const movieIdx = this.movies.findIndex(m => m.id === id);
+    if (movieIdx === -1) {
+      throw new NotFoundException(`Movie with ID ${id} not found`);
+    }
+    this.movies.splice(movieIdx, 1);
+    return movieIdx
+  } 
+  
+  
+
+  
 }
