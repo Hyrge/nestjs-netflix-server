c267e7d922406a776c2ac82c58ba9ac197f3f148
Hyrge <ekek7478@gmail.com>
2025-07-06 05:50:14 +0900

    chore: AppModule에 주석 추가하여 모듈 역할 설명

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [MovieModule],
+  imports: [MovieModule], // 나중에 모든 모듈이 집합하는 중앙 역할만 함 
 })
 export class AppModule {}
